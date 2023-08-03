5ee789c5cb5b3a09a613448219aa95e68cff6a3e
Ayushman Chhabra <info@ayushmanchhabra.com>
2023-08-03 22:53:16 +0530

    Generate Ninja files

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # nw-build-tools
 
+> Currently only Windows platform is supported.
+
 NW.js local development
+
+## Getting Started
+
+1. Setup your Windows environment (link in Resources 1)
+1. Clone the repo
+1. `corepack enable` (or `npm i -g corepack` if not installed)
+1. `pnpm i`
+1. `pnpm start`
+
+## Resources
+
+1. [Setup Windows environment](https://chromium.googlesource.com/chromium/src/+/main/docs/windows_build_instructions.md#Setting-up-Windows)
+
+1. [Modify Visual Studio Installer via command line](https://learn.microsoft.com/en-us/visualstudio/install/use-command-line-parameters-to-install-visual-studio?view=vs-2022#install-update-modify-repair-uninstall-and-export-commands-and-command-line-parameters)
--- main.mjs
+++ main.mjs
@@ -13,42 +13,57 @@ await main();
  * @return {Promise<unknown>}
  */
 function exec(code, ...args) {
-    return new Promise((res) => {
-        const proc = spawn(code, args, {
-            stdio: "inherit",
-            shell: true,
-            env: {
-                ...env,
-                Path: env.PATH,
-            },
-        });
-        proc.addListener("exit", res);
+  return new Promise((res) => {
+    const proc = spawn(code, args, {
+      stdio: "inherit",
+      shell: true,
+      env: {
+        ...env,
+        Path: env.PATH,
+      },
     });
+    proc.addListener("exit", res);
+  });
 }
 
 async function main() {
+  const INSTALL_VS = false;
+  if (INSTALL_VS) {
+    const VS_INSTALLER_PATH = resolve(
+      "/",
+      '"Program Files (x86)"',
+      '"Microsoft Visual Studio"',
+      "Installer",
+      "setup.exe",
+    );
 
-    const VS_INSTALLER_PATH = resolve("\\", "Program Files", "Microsoft Visual Studio", "Installer", "vs_installer.exe");
     await exec(
-        `${VS_INSTALLER_PATH} ^`,
-        "--add Microsoft.VisualStudio.Workload.NativeDesktop ^",
-        "--add Microsoft.VisualStudio.Component.VC.ATLMFC ^",
-        "--includeRecommended",
-        );
+      `${VS_INSTALLER_PATH}`,
+      "install",
+      "--channelId VisualStudio.17.Release",
+      "--productId Microsoft.VisualStudio.Product.Community",
+      "--add Microsoft.VisualStudio.Workload.NativeDesktop",
+      "--add Microsoft.VisualStudio.Component.VC.ATLMFC",
+      "--add Microsoft.VisualStudio.Component.VC.v143.x86.x64",
+      "--includeRecommended",
+      "--installWhileDownloading",
+      "--quiet",
+    );
+  }
 
-    if (existsSync("./third_party/depot_tools") === false) {
-        await exec(
-            "git",
-            "clone",
-            "https://chromium.googlesource.com/chromium/tools/depot_tools.git",
-            "third_party/depot_tools",
-        );
-    }
+  if (existsSync("./third_party/depot_tools") === false) {
+    await exec(
+      "git",
+      "clone",
+      "https://chromium.googlesource.com/chromium/tools/depot_tools.git",
+      "third_party/depot_tools",
+    );
+  }
 
-    env.DEPOT_TOOLS_WIN_TOOLCHAIN = "0";
-    env.PATH = `${resolve("./third_party/depot_tools")};${env.PATH};`;
+  env.DEPOT_TOOLS_WIN_TOOLCHAIN = "0";
+  env.PATH = `${resolve("./third_party/depot_tools")};${env.PATH};`;
 
-    const gclient = `
+  const gclient = `
 solutions = [
     { "name"        : 'src',
         "url"         : 'https://github.com/ayushmanchhabra/nw-chromium.git@origin/nw78',
@@ -58,30 +73,50 @@ solutions = [
         "custom_vars": {},
     },
 ]`;
-    
-    await writeFile("./.gclient", gclient);
-    
-    if (existsSync("./src") === false) {
-        await mkdir("./src");
-        await exec("gclient", "sync", "--no-history", "--nohooks");
-    }
-    
-    await chdir("./src");
-    
-    if (existsSync("content/nw") === false) {
-        await exec("git", "clone", "--branch=nw78", "--depth=1", "https://github.com/ayushmanchhabra/nw-core.git", "content/nw");
-    }
 
-    if (existsSync("third_party/node-nw") === false) {
-        await exec("git", "clone", "--branch=nw78", "--depth=1", "https://github.com/ayushmanchhabra/nw-node.git", "third_party/node-nw");
-    }
+  await writeFile("./.gclient", gclient);
 
-    if (existsSync("v8") === false) {
-        await exec("git", "clone", "--branch=nw78", "--depth=1", "https://github.com/ayushmanchhabra/nw-v8.git", "v8");
-    }
+  if (existsSync("./src") === false) {
+    await mkdir("./src");
+    await exec("gclient", "sync", "--no-history", "--nohooks");
+  }
 
-    await exec("gclient", "sync");
+  await chdir("./src");
 
-    // await exec("gn", "gen", "out/nw");
-    
+  if (existsSync("content/nw") === false) {
+    await exec(
+      "git",
+      "clone",
+      "--branch=nw78",
+      "--depth=1",
+      "https://github.com/ayushmanchhabra/nw-core.git",
+      "content/nw",
+    );
+  }
+
+  if (existsSync("third_party/node-nw") === false) {
+    await exec(
+      "git",
+      "clone",
+      "--branch=nw78",
+      "--depth=1",
+      "https://github.com/ayushmanchhabra/nw-node.git",
+      "third_party/node-nw",
+    );
+  }
+
+  if (existsSync("v8") === false) {
+    await exec(
+      "git",
+      "clone",
+      "--branch=nw78",
+      "--depth=1",
+      "https://github.com/ayushmanchhabra/nw-v8.git",
+      "v8",
+    );
+  }
+
+  await exec("gclient", "sync");
+
+  await exec("gn", "gen", "out/nw");
 }
